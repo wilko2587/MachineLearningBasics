18287cdf6880c9e2049d9925e4846fafd2337a9f
james-wilkinson-NW <91038008+james-wilkinson-NW@users.noreply.github.com>
2021-11-21 13:11:59 -0600

    2 new features:
    1) Added in functionality for L1 and L2 regularisation + elastic nets (combination of both).
    
    2) added in "methods" for all the neural network's layers. EG: sigmoid in the hidden layer. Defaults to sigmoid. Now we use pytorch's own softmax function

--- Project/feedforward.py
+++ Project/feedforward.py
@@ -8,20 +8,23 @@ import transformation_utils as tu
 import validation_utils as vu
 import copy
 
+
 def softmax(x):
     '''
+    DEPRECIATED: now using pytorch's built in softmax
     returns softmax of x
     '''
-    #x = np.array(x.detach().numpy()) #convert to number
+    # x = np.array(x.detach().numpy()) #convert to number
     Ndims = len(x.shape)
     if Ndims == 2:
-        return torch.div(torch.exp(x),torch.sum(torch.exp(x),1).unsqueeze(1))
+        return torch.div(torch.exp(x), torch.sum(torch.exp(x), 1).unsqueeze(1))
     elif Ndims == 1:
-        return torch.div(torch.exp(x),torch.sum(torch.exp(x)))
+        return torch.div(torch.exp(x), torch.sum(torch.exp(x)))
 
 
 class FeedForwardSoftmax(nn.Module):
-    def __init__(self, inputN, outputN, hiddenNs=None, bias=True):
+    def __init__(self, inputN, outputN, hiddenNs=None, bias=True,
+                 inputMethod=nn.Sigmoid, hiddenMethods=[], outputMethod=nn.Softmax):
         """
         Feed-forward linear neural network, using softmax at the output layer to normalise the outputs.
 
@@ -30,17 +33,29 @@ class FeedForwardSoftmax(nn.Module):
                        us the size of that layer
         :param: outputN = integer. Nuber of output nodes for our network
         :param: bias = True/False bool. If True pytorch will add a bias term into the calculations
+        :param: inputMethod = nn.Module object. transformation to apply to the input layer
+        :param: hiddenMethods = list of nn.Module objects. transformation to apply to the hidden layers
+        :param: outputMethod = nn.Module object. transformation to apply to the output layer
+
         """
 
         super(FeedForwardSoftmax, self).__init__()
 
         if hiddenNs is None:
             hiddenNs = []
+        if len(hiddenNs) != 0 and len(hiddenMethods) == 0:
+            hiddenMethods = [nn.Sigmoid] * len(hiddenNs)  # default to Sigmoid in hidden layers if not specified
 
         layer_sizes = [inputN] + hiddenNs + [outputN]  # form list of all layer sizes in the network
         self._layer_sizes = layer_sizes
 
         self._layers = nn.ModuleList()  # initialise container for the layer objects
+
+        # initialise container for the methods list (transformation for each layer, eg: sigmoid)
+        self._methods = [inputMethod()] \
+                        + [x() for x in hiddenMethods] \
+                        + [outputMethod()]
+
         for i in range(len(layer_sizes) - 1):  # build the architecture
             self._layers.append(nn.Linear(layer_sizes[i], layer_sizes[i + 1], bias=bias))
 
@@ -52,12 +67,13 @@ class FeedForwardSoftmax(nn.Module):
         :param: x = input vector to the network
         returns network output
         """
-        #assert our vector x is a tensor
+        # assert our vector x is a tensor
         x = tensor(np.array(x)).to(torch.float32)
 
-        for i in range(len(self._layers)): #iterate through the layers, passing x from one layer to the next
+        for i in range(len(self._layers)):  # iterate through the layers, passing x from one layer to the next
             x = self._layers[i](x)
-        return softmax(x)
+            x = self._methods[i](x)
+        return x
 
     def get_weights(self):
         '''
@@ -69,9 +85,9 @@ class FeedForwardSoftmax(nn.Module):
         return weights
 
 
-def trainNN(dataset, model, loss_func, optimizer, max_epoch = 10000,
-            loss_target = 0.1, method = "batch", plot=True, verbosity=True,
-            _lambda = 0):
+def trainNN(dataset, model, loss_func, optimizer, max_epoch=10000,
+            loss_target=0.1, method="batch", plot=True, verbosity=True,
+            _lambdaL1=0, _lambdaL2=0):
     '''
     takes a dataset, and a model (such as FeedForwardSoftmax), a loss function, and a
     pytorch optimizer and trains the model using a batch method
@@ -84,10 +100,11 @@ def trainNN(dataset, model, loss_func, optimizer, max_epoch = 10000,
     :param: loss_target: the target loss we're aiming for -> if this is reached the training will stop
     :param: method: either "batch" or "stochastic".
     :poram: plot: True/False. If true, matplotlib called to plot the loss vs epoch
-    :param: _lambda: the regularisation constant for L2
+    :param: _lambdaL2: the regularisation constant for L2
+    :param: _lambdaL1: the regularisation constant for L1
     '''
 
-    model.train() # tell the model we're training
+    model.train()  # tell the model we're training
     train_loss = []
 
     Noutputs = model._layer_sizes[-1]
@@ -98,21 +115,21 @@ def trainNN(dataset, model, loss_func, optimizer, max_epoch = 10000,
 
     ybin = tu.labels_to_binary(y, Noutputs)
 
-    full_loss = 1e8 # initialise to a value somewhere above the threshold
-    epoch = 0 # counter for which training epoch we are in
+    full_loss = 1e8  # initialise to a value somewhere above the threshold
+    epoch = 0  # counter for which training epoch we are in
     if verbosity:
         print('Training {} using method: {}'.format(type(model).__name__, method))
 
     while full_loss > loss_target and epoch < max_epoch:
-        if method.lower() == "batch": #if batch -> use all the training data in each iteration
+        if method.lower() == "batch":  # if batch -> use all the training data in each iteration
             _X = X
             _y = ybin
-        elif method.lower() == 'stochastic': #if stochastic -> use one randomly selected example for each epoch
-            randomi = randint(0, len(X)-1)
+        elif method.lower() == 'stochastic':  # if stochastic -> use one randomly selected example for each epoch
+            randomi = randint(0, len(X) - 1)
             _X = X[randomi]
             _y = ybin[randomi]
         else:
-            raise(NameError("Kwarg 'method' must be either 'batch' or 'stochastic'"))
+            raise (NameError("Kwarg 'method' must be either 'batch' or 'stochastic'"))
 
         # make predictions
         pred = model.forward(_X)
@@ -120,9 +137,10 @@ def trainNN(dataset, model, loss_func, optimizer, max_epoch = 10000,
         # calculate the loss
         loss = loss_func(pred.unsqueeze(0), _y.unsqueeze(0))
 
-        #deal with regularization
+        # deal with regularization
         l2_norm = sum(p.pow(2.0).sum() for p in model.parameters()).item()
-        loss = loss + (_lambda*l2_norm)
+        l1_norm = sum(p.abs().sum() for p in model.parameters()).item()
+        loss = loss + (_lambdaL2 * l2_norm) + (_lambdaL1 * l1_norm)
 
         # backprop
         optimizer.zero_grad()
@@ -136,7 +154,7 @@ def trainNN(dataset, model, loss_func, optimizer, max_epoch = 10000,
         if epoch % 50 == 0 and verbosity:
             print(
                 'epoch: {} | loss: {} | target: {}'.format(
-                    epoch, round(full_loss,4), loss_target), end="\r"
+                    epoch, round(full_loss, 4), loss_target), end="\r"
             )
 
     if full_loss <= loss_target:
@@ -147,7 +165,7 @@ def trainNN(dataset, model, loss_func, optimizer, max_epoch = 10000,
         reason = "max epoch reached ({})".format(max_epoch)
 
     if verbosity:
-        print("\nTraining complete! : {}".format(reason)) # print we're complete and reason the training stopped
+        print("\nTraining complete! : {}".format(reason))  # print we're complete and reason the training stopped
         print(
             'Final epoch: {} | Final loss: {} | target: {}'.format(
                 epoch, round(full_loss, 4), loss_target)
@@ -164,14 +182,14 @@ def trainNN(dataset, model, loss_func, optimizer, max_epoch = 10000,
 
 
 def generate_learning_curve(train, valid, model, loss_func, optimizer, max_epoch,
-                            method="batch", plot=True, _lambda = 0):
+                            method="batch", plot=True, _lambda=0):
     '''
     for a model, with train data and valid data + other params, plot a training curve using loss as
     the metric. Plots training epoch vs loss separately for training data and validation data.
     '''
 
-    epochs = range(1,max_epoch,1000)
-    D_epoch = epochs[1]-epochs[0]
+    epochs = range(1, max_epoch, 1000)
+    D_epoch = epochs[1] - epochs[0]
 
     Noutputs = model._layer_sizes[-1]
 
@@ -183,39 +201,39 @@ def generate_learning_curve(train, valid, model, loss_func, optimizer, max_epoch
     yvalid = valid[1]
     yvalid_bin = tu.labels_to_binary(yvalid, Noutputs)
 
-    train_losses = [] #containers to plot learning curves
+    train_losses = []  # containers to plot learning curves
     valid_losses = []
     print('Generating Learning Curves for {}'.format(type(model).__name__))
     for each in epochs:
         # train our model for another D_epochs using training data.
         # Set loss_target negative so its guaranteed to train for D_epochs
-        trainNN(copy.deepcopy(train),model,loss_func,optimizer,
-                max_epoch = D_epoch,
-                loss_target = -1,
-                method = method,
+        trainNN(copy.deepcopy(train), model, loss_func, optimizer,
+                max_epoch=D_epoch,
+                loss_target=-1,
+                method=method,
                 plot=False,
                 verbosity=False,
-                _lambda = _lambda)
+                _lambda=_lambda)
 
-        #calculate training loss
+        # calculate training loss
         train_preds = model.forward(Xtrain)
-        train_loss = loss_func(ytrain_bin,train_preds).item()
+        train_loss = loss_func(ytrain_bin, train_preds).item()
 
-        #calculate validation loss
+        # calculate validation loss
         valid_preds = model.forward(Xvalid)
-        valid_loss = loss_func(yvalid_bin,valid_preds).item()
+        valid_loss = loss_func(yvalid_bin, valid_preds).item()
 
         train_losses.append(train_loss)
         valid_losses.append(valid_loss)
 
         print('Epoch: {} | training acc: {} | validation acc: {}'.format(
-            each,round(train_loss,4),round(valid_loss,4)),end='\r')
+            each, round(train_loss, 4), round(valid_loss, 4)), end='\r')
     print('\n')
 
     if plot:
         f = plt.figure()
-        plt.plot(epochs,train_losses,label='training loss')
-        plt.plot(epochs,valid_losses,label='validation loss')
+        plt.plot(epochs, train_losses, label='training loss')
+        plt.plot(epochs, valid_losses, label='validation loss')
         plt.xlabel('Training Epoch')
         plt.ylabel('Loss')
         plt.suptitle("Learning Curve")
--- Project/main.py
+++ Project/main.py
@@ -6,20 +6,24 @@ import torch
 
 catdata = dr.read_cat()
 
-train, valid, test = dr.generate_sets(catdata, splits=[70,15,15])
-train, valid, test = tu.scale01(train,[train,valid,test])
+train, valid, test = dr.generate_sets(catdata, splits=[70, 15, 15])
+train, valid, test = tu.scale01(train, [train, valid, test])
 
-#lets make a simple feed forward NN with one hidden layer, softmax output
-net = ff.FeedForwardSoftmax(len(train[0][0]), 3, hiddenNs = [3]) # N x 3 x 3 neural net
+# lets make a simple feed forward NN with one hidden layer, softmax output
+net = ff.FeedForwardSoftmax(len(train[0][0]), 3, hiddenNs=[3])  # N x 3 x 3 neural net
 loss = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(net.parameters(), lr=1e-3, momentum=0.9)
 
-losses = ff.trainNN(train,net,loss,optimizer,
-                    max_epoch = 100000,
-                    loss_target = 0.74,
-                    method = 'stochastic',
-                    plot = True,
-                    verbosity = True,
-                    _lambda = 0)
+losses = ff.trainNN(train, net, loss, optimizer,
+                    max_epoch=100000,
+                    loss_target=0.72,
+                    method='batch',  # pick "batch" or "stochastic"
+                    plot=True,
+                    verbosity=True,
+
+                    # L1 and L2 regularisation strengths. NB: you can use a combination of both - this is called an
+                    # elastic net
+                    _lambdaL1=0.01,
+                    _lambdaL2=0)
 
 ### because we have a lot of features, some that may not be relevant, i want to try L1 regularization
