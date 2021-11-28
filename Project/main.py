b7b09094581ed9769d6f9010270c004f70f330e5
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-28 13:22:34 -0600

    updated models function

--- Project/main.py
+++ Project/main.py
@@ -8,8 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.mixture import GaussianMixture
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
 import numpy as np
 import pandas as pd
 
@@ -78,135 +77,68 @@ def neural_net():
 
 def models():
     '''
-    I removed id (not needed for any analysis), as well as bnp, a1c, and chol because of high degree of missingness.
-    Transformed gender and smoking to numeric.
-    Imputed missing values as mean. Scaled data to mean 0, same SD as prior.
-    Ran 5 models: baseline RF, RF with not important elements removed, KNN, Gaussian mixture model, and gradient boosting classifier
-    All stink.
+    Beginning is all data prep.
+    After this, data stored as (train_df, test_df) and labels as (train_labels, test_labels)
     '''
 
-    # Load data, drop irrelevant or missing, convert to numeric
     data = dr.read('deid_full_data_cont.csv')
-    exclude = ['id','bnp','a1c','chol'] # drop id because it isn't useful, values with high missingness
+
+    exclude = ['id','bnp','a1c','chol'] # drop id because it isn't useful, values highly missing
     data.drop(columns=exclude, axis=1, inplace=True)
-    data['gender'].replace(to_replace=['Male', 'Female',], value=[0, 1], inplace=True) # make numeric
+
+    data['gender'].replace(to_replace=['Male', 'Female',], value=[0, 1], inplace=True) # make gender/smoke numeric
     smoke_cat = data['smoke'].unique()
     data['smoke'].replace(to_replace=smoke_cat, value=np.arange(11), inplace=True)
 
-    # 80/20 train/test split
     train = data.sample(frac=0.8, random_state=2)
     test = data.drop(train.index)
 
-    train_data, train_Y = dr.split_hyperparams_target(train,'stage')
+    train_data, train_Y = dr.split_hyperparams_target(train,'stage') # split data from label
     test_data, test_Y = dr.split_hyperparams_target(test,'stage')
 
     final_col = train_data.columns # saving column names for later
 
-    # Putting labels in a format sklearn needs
-    train_labels = [x[0] for x in train_Y.to_numpy()]
-    test_labels = [x[0] for x in test_Y.to_numpy()]
-
-    # Imputing values
-    simp_imp = SimpleImputer(strategy='mean').fit(train_data)
+    simp_imp = SimpleImputer(strategy='mean').fit(train_data) # impute missing values as mean
     train_imp = simp_imp.transform(train_data)
     test_imp = simp_imp.transform(test_data)
 
-    # Scaling
-    scaler = StandardScaler().fit(train_imp)
+    scaler = StandardScaler().fit(train_imp) # scale values to mean 0, perserve variance
     train_clean = scaler.transform(train_imp)
     test_clean = scaler.transform(test_imp)
 
-    # Putting things back as a df
-    train_df = pd.DataFrame(train_clean)
+    train_df = pd.DataFrame(train_clean) # make things df again
     test_df = pd.DataFrame(test_clean)
     train_df.columns = final_col
     test_df.columns = final_col
 
-    # RF model
+    train_labels = [x[0] for x in train_Y.to_numpy()] # convert to format sklearn likes
+    test_labels = [x[0] for x in test_Y.to_numpy()]
+
+#########################################################################################
+
+    # Now try different models
     rf = RandomForestClassifier(random_state = 0, n_estimators=100)
-    rf.fit(train_df,train_labels)
-    predicts = rf.predict(test_df)
+    knn =  KNeighborsClassifier(n_neighbors=5)
+    log = LogisticRegression(random_state=0)
 
-    # Model Performance
-    predicts_tensor = torch.tensor(predicts)
-    test_labels_tensor = torch.tensor(test_labels)
-    conf_matrix = vu.confusion_matrix(predicts_tensor, test_labels_tensor)
-    classes = {0:'Not HF', 1: 'Stage C', 2: 'Stage D'}
+    models = [rf, knn,log]
 
-    print('Baseline RF')
-    for each in classes:
-        precision, recall, f1 = vu.precision_recall_F1(conf_matrix,each)
-        print(f'{classes[each]}: Precision: {round(precision,2)}, Recall: {round(recall,2)}, F1: {round(f1,2)}')
+    for model in models:
+        model.fit(train_df, train_labels)
+        predicts = model.predict(test_df)
 
-    # RF model 2 - tried removing unimportant features, didn't help
-    not_important = list()
+        predicts_tensor = torch.tensor(predicts)
+        test_labels_tensor = torch.tensor(test_labels)
+        conf_matrix = vu.confusion_matrix(predicts_tensor, test_labels_tensor)
+        classes = {0:'Not HF', 1: 'Stage C', 2: 'Stage D'}
 
-    for x,y in zip(train_df,rf.feature_importances_):
-        if y < 0.05:
-            not_important.append(x)
+        print(model)
 
-    train_trim_df = train_df.drop(not_important,axis=1)
-    test_trim_df = test_df.drop(not_important,axis=1)
-    rf2 = RandomForestClassifier(random_state = 0, n_estimators=100)
-    rf2.fit(train_trim_df,train_labels)
-    predicts2 = rf2.predict(test_trim_df)
+        for each in classes:
+            precision, recall, f1 = vu.precision_recall_F1(conf_matrix,each)
+            print(f'{classes[each]}: Precision: {round(precision,2)}, Recall: {round(recall,2)}, F1: {round(f1,2)}')
 
-    # Trimmed RF Performance
-    predicts_tensor2 = torch.tensor(predicts2)
-    conf_matrix2 = vu.confusion_matrix(predicts_tensor2, test_labels_tensor)
-
-    print('RF trimmed')
-
-    for each in classes:
-        precision, recall, f1 = vu.precision_recall_F1(conf_matrix2,each)
-        print(f'{classes[each]}: Precision: {round(precision,2)}, Recall: {round(recall,2)}, F1: {round(f1,2)}')
-
-    # K nearest neighbors (k=5, all choices were bad)
-    neigh = KNeighborsClassifier(n_neighbors=5)
-    neigh.fit(train_df,train_labels)
-    predicts3 = neigh.predict(test_df)
-
-    # KNN Performance
-    predicts_tensor3 = torch.tensor(predicts3)
-    test_labels_tensor3 = torch.tensor(test_labels)
-    conf_matrix3 = vu.confusion_matrix(predicts_tensor3, test_labels_tensor3)
-
-    print('k=5')
-
-    for each in classes:
-        precision, recall, f1 = vu.precision_recall_F1(conf_matrix3, each)
-        print(f'{classes[each]}: Precision: {round(precision, 2)}, Recall: {round(recall, 2)}, F1: {round(f1, 2)}')
-
-    # Gaussian mixture model
-    gaus = GaussianMixture(n_components=3,random_state=0)
-    gaus.fit(train_df,train_labels)
-    predicts4 = gaus.predict(test_df)
-
-    # Model Performance
-    predicts_tensor4 = torch.tensor(predicts4)
-    conf_matrix4 = vu.confusion_matrix(predicts_tensor4, test_labels_tensor)
-
-    print('Gaussian mixture model')
-
-    for each in classes:
-        precision, recall, f1 = vu.precision_recall_F1(conf_matrix4, each)
-        print(f'{classes[each]}: Precision: {round(precision, 2)}, Recall: {round(recall, 2)}, F1: {round(f1, 2)}')
-
-    # Gradient boosting classifier
-    gbm = GradientBoostingClassifier(n_estimators=1000, learning_rate=1, max_depth=3, random_state=0)
-    gbm.fit(train_df,train_labels)
-    predicts5 = gbm.predict(test_df)
-
-    # Model Performance
-    predicts_tensor5 = torch.tensor(predicts5)
-    conf_matrix5 = vu.confusion_matrix(predicts_tensor5, test_labels_tensor)
-
-    print('gradient boosting classifier')
-
-    for each in classes:
-        precision, recall, f1 = vu.precision_recall_F1(conf_matrix4, each)
-        print(f'{classes[each]}: Precision: {round(precision, 2)}, Recall: {round(recall, 2)}, F1: {round(f1, 2)}')
-
+        print('----------------------------------------------------------------')
 
 def univariate():
     '''
@@ -226,5 +158,5 @@ def univariate():
 
 if __name__ == '__main__':
     # univariate()
-    neural_net()
-    # models() #RF, RF trimmed, KNN, Gaussian mixture model, gradient boosting classifier
+    # neural_net()
+    models() # trying different models in sklearn. you guys can tweak this easily
