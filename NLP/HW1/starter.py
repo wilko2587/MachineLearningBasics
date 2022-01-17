a08137299b0f3432960121d2bd54378c452df31e
wilko2587 <37017969+wilko2587@users.noreply.github.com>
2022-01-17 15:29:23 -0600

    A basic corpus built, with encode_as_ints and encode_as_text

--- NLP/HW1/starter.py
+++ NLP/HW1/starter.py
@@ -10,23 +10,40 @@ class my_corpus():
         
         self.params = params
         print('setting parameters')
-    
+
+        print('building corpus')
+        with open('source_text.txt') as f:
+            lines = f.readlines()
+
+        fulltext = ' '.join(lines).lower()
+
+        tokens = nltk.word_tokenize(fulltext)
+        unique_tokens = list(set(tokens))
+        self._tokens = tokens
+        self._tokenmap = {unique_tokens[i]:i for i in range(len(unique_tokens))}
+
     def encode_as_ints(self, sequence):
         
         int_represent = []
         
         print('encode this sequence: %s' % sequence)
         print('as a list of integers.')
-        
+        tokens = nltk.word_tokenize(sequence)
+        for t in tokens:
+            int_represent.append(self._tokenmap[t])
         return(int_represent)
     
-    def encode_as_text(self,int_represent):
+    def encode_as_text(self, int_represent):
 
         text = ''
         
         print('encode this list', int_represent)
         print('as a text sequence.')
-        
+
+        inv_map = {v: k for k, v in self._tokenmap.items()} #inverted self._tokenmap (switch values/keys)
+        for i in int_represent:
+            token = inv_map[i]
+            text = text + ' ' + token
         return(text)
 
 def tokens(sequence):
@@ -52,7 +69,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-        
-    
-    
-              
