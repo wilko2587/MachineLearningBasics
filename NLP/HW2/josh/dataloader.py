008277bafe30a790c482ff0e686559249750cb06
wilko2587 <37017969+wilko2587@users.noreply.github.com>
2022-02-09 17:37:20 -0600

    Took out accuracy calcs to speed up (think thats all I did)

--- NLP/HW2/josh/dataloader.py
+++ NLP/HW2/josh/dataloader.py
@@ -9,10 +9,10 @@ class wiki_dataloader(pl.LightningDataModule):
         self.train_dataset = datasets[0]
         self.valid_dataset = datasets[1]
         self.test_dataset = datasets[2]
+
         self.token_map = datasets[0].token_map
         self.batch_size = batch_size
 
-    def setup(self, stage=None):
         self.train = [[each[0],each[1]] for each in self.train_dataset]
         self.val = [[each[0],each[1]] for each in self.valid_dataset]
         self.test = [[each[0],each[1]] for each in self.test_dataset]
--- NLP/HW2/josh/feedforward.py
+++ NLP/HW2/josh/feedforward.py
@@ -8,23 +8,28 @@ from dataloader import wiki_dataloader
 import torchmetrics
 import pytorch_lightning as pl
 import pytorch_lightning.loggers as pl_loggers
+import time
 import matplotlib.pyplot as plt
+
 
 class FeedForward(pl.LightningModule):
 
     def __init__(self, context, embed_dim, vocab_size):
         super(FeedForward, self).__init__()
-        self.embed = nn.Embedding(vocab_size,embed_dim)
-        self.lin1 = nn.Linear(context*embed_dim,1000)
-        self.drop1 = nn.Dropout(p=0.5)
-        self.lin2 = nn.Linear(1000,vocab_size)
-        self.loss = nn.CrossEntropyLoss()
+        self.embed = nn.Embedding(vocab_size, embed_dim)
+        self.lin1 = nn.Linear(context * embed_dim, 1000)
+        #self.drop1 = nn.Dropout(p=0.5)
+        self.lin2 = nn.Linear(1000, vocab_size)
+
+        #l2_norm = sum(p.pow(2.0).sum() for p in self.parameters()).item()
+
+        self.loss = nn.CrossEntropyLoss() #+ l2_norm
 
     def forward(self, X):
         X = self.embed(X)
-        X = torch.flatten(X,start_dim=1)
+        X = torch.flatten(X, start_dim=1)
         X = torch.tanh(self.lin1(X))
-        X = self.drop1(X)
+        #X = self.drop1(X)
         X = self.lin2(X)
         return X
 
@@ -32,51 +37,44 @@ class FeedForward(pl.LightningModule):
         return optim.Adam(self.parameters(), lr=1e-3)
 
     def training_step(self, batch, batch_idx):
-        data,label = batch
+        data, label = batch
         logits = self.forward(data)
-        # l2_norm = sum(p.pow(2.0).sum() for p in self.parameters()).item()
-        loss = self.loss(logits,label)
-        accuracy = torchmetrics.functional.accuracy(logits,label)
-        tensorboard_logs = {'acc': {'train': accuracy.detach()}, 'loss': {'train': loss.detach()}}
+        loss = self.loss(logits, label)
+        tensorboard_logs = {'loss': {'train': loss.detach()}}
         self.log("training loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-        self.log("training accuracy", accuracy, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         return {"loss": loss, "log": tensorboard_logs}
 
     def validation_step(self, batch, batch_idx):
-        data,label = batch
+        data, label = batch
         logits = self.forward(data)
-        loss = self.loss(logits,label)
-        accuracy = torchmetrics.functional.accuracy(logits,label)
-        tensorboard_logs = {'acc': {'val': accuracy.detach()}, 'loss': {'val': loss.detach()}}
+        loss = self.loss(logits, label)
+        tensorboard_logs = {'loss': {'val': loss.detach()}}
         self.log("validation loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-        self.log("validation accuracy", accuracy, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         return {"loss": loss, "log": tensorboard_logs}
 
     def test_step(self, batch, batch_idx):
-        data,label = batch
+        data, label = batch
         logits = self.forward(data)
-        loss = self.loss(logits,label)
-        accuracy = torchmetrics.functional.accuracy(logits,label)
-        tensorboard_logs = {'acc': {'test': accuracy.detach()}, 'loss': {'test': loss.detach()}}
+        loss = self.loss(logits, label)
+        tensorboard_logs = {'loss': {'test': loss.detach()}}
         self.log("test loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-        self.log("test accuracy", accuracy, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         return {"loss": loss, "log": tensorboard_logs}
+
 
 if __name__ == '__main__':
     # Load datasets
     train = wiki_dataset('../wiki.train.txt', training=True, token_map='create')
     valid = wiki_dataset('../wiki.valid.txt', training=False, token_map=train.token_map)
     test = wiki_dataset('../wiki.test.txt', training=False, token_map=train.token_map)
-    datasets = [train,valid,test]
+    datasets = [train, valid, test]
 
     # Load dataloader
-    dataloader = wiki_dataloader(datasets=datasets, batch_size=5000)
+    dataloader = wiki_dataloader(datasets=datasets, batch_size=20)
 
     # Make model and train
-    model = FeedForward(context=train.window, embed_dim=10, vocab_size=len(train.unique_tokens))
+    model = FeedForward(context=train.window, embed_dim=100, vocab_size=len(train.unique_tokens))
     tb_logger = pl_loggers.TensorBoardLogger("./lightning_logs/", name="ff")
     trainer = pl.Trainer(logger=tb_logger, max_epochs=10)
     trainer.fit(model, dataloader)
     result = trainer.test(model, dataloader)
     print(result)
-
