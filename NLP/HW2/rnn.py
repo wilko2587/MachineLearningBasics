f4b0d081906005ea509ec698df5a91c64edd19c9
baljash-cheema <baljash-cheema@northwestern.edu>
2022-02-14 14:02:14 -0600

    removed passing hidden state btw batches

--- NLP/HW2/rnn.py
+++ NLP/HW2/rnn.py
@@ -14,42 +14,34 @@ import matplotlib.pyplot as plt
 class rnn(pl.LightningModule):
     def __init__(self, n_vocab, embedding_size, hidden_size, num_layers):
         super(rnn, self).__init__()
-        self.hidden_state = None
         self.embed = nn.Embedding(n_vocab, embedding_size)
-        self.rnn = nn.RNN(input_size=embedding_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.rnn = nn.RNN(input_size=embedding_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=False, dropout=0.5)
         self.fc = nn.Linear(hidden_size, n_vocab)
         self.loss = nn.CrossEntropyLoss()
 
-    def forward(self, x, hidden_state):
+    def forward(self, x):
         x = self.embed(x)
-        print(f'initial x {x.shape}')
         x, hidden = self.rnn(x)
-        print(f'after rnn x {x.shape}')
-        print(f'after rnn hidden {hidden.shape}')
         x = x[:, -1, :]
-        print(f'after reshape x {x.shape}')
         logits = self.fc(x)
-        print(f'after linear x {x.shape}')
 
-        return logits, hidden
+        return logits
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=1e-3)
 
     def training_step(self, batch, batch_idx):
         data, label = batch
-        logits, self.hidden_state = self.forward(data, self.hidden_state)
-        self.hidden_state = [state.detach() for state in self.hidden_state] #holding onto the numbers, not the gradient
-        # l2_norm = sum(p.pow(2.0).sum() for p in self.parameters()).item()
-        # l1_norm = sum(p.abs().sum() for p in self.parameters()).item()
-        loss = self.loss(logits, label)# + 0.001*l2_norm + 0.001*l1_norm
+        logits = self.forward(data)
+        # self.hidden_state = [state.detach() for state in self.hidden_state] #holding onto the numbers, not the gradient
+        loss = self.loss(logits, label)
         tensorboard_logs = {'loss': {'train': loss.detach()}}
         self.log("training loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         return {"loss": loss, "log": tensorboard_logs}
 
     def validation_step(self, batch, batch_idx):
         data, label = batch
-        logits, self.hidden_state = self.forward(data, self.hidden_state)
+        logits = self.forward(data)
         loss = self.loss(logits, label)
         tensorboard_logs = {'loss': {'val': loss.detach()}}
         self.log("validation loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
@@ -57,7 +49,7 @@ class rnn(pl.LightningModule):
 
     def test_step(self, batch, batch_idx):
         data, label = batch
-        logits, self.hidden_state = self.forward(data, self.hidden_state)
+        logits = self.forward(data)
         loss = self.loss(logits, label)
         tensorboard_logs = {'loss': {'test': loss.detach()}}
         self.log("test loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
