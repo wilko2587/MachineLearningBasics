7de19471df280b39e9cfc4dd28c738d32e1eb627
james-wilkinson-NW <91038008+james-wilkinson-NW@users.noreply.github.com>
2022-02-16 16:58:56 -0600

    Multi-labels implemented (needs debug)
    hidden state retention completed (needs debug)

--- NLP/HW2/LSTM.py
+++ NLP/HW2/LSTM.py
@@ -16,31 +16,33 @@ class LSTM1(pl.LightningModule):
                  embedding_size,
                  num_layers,
                  dropout=0,
-                 lr = 1e-3,
+                 lr=1e-3,
                  trainweights=None):
-
         super(LSTM1, self).__init__()
 
         self.embed = nn.Embedding(n_vocab, embedding_size)
 
-        self.lstm = nn.LSTM(input_size = embedding_size,
-                            hidden_size = embedding_size,
-                            num_layers = num_layers,
+        self.lstm = nn.LSTM(input_size=embedding_size,
+                            hidden_size=embedding_size,
+                            num_layers=num_layers,
                             batch_first=False, dropout=dropout)
 
-        self.fc = nn.Linear(embedding_size, n_vocab) #transpose of embedding layer; need same weights but transposed
-        self.fc.weight = self.embed.weight # tie embeddings
+        self.fc = nn.Linear(embedding_size, n_vocab)  # transpose of embedding layer; need same weights but transposed
+        self.fc.weight = self.embed.weight  # tie embeddings
 
         self.loss = nn.CrossEntropyLoss(weight=trainweights)
-        self.viewloss = nn.CrossEntropyLoss() # weights change the results of the loss, so we initialise an unweighted
-                                                # loss to keep track and use for perplexity calcs
+        self.viewloss = nn.CrossEntropyLoss()  # weights change the results of the loss, so we initialise an unweighted
+        # loss to keep track and use for perplexity calcs
         self.lr = lr
+
+        self.state = None # initialise
 
     def forward(self, x):
         x = self.embed(x)
-        x, _ = self.lstm(x)
-        x = x[:, -1, :]
-        logits = self.fc(x) #logit from running x through linear layer
+        x, ht, ct = self.lstm(x, self.state[0], self.state[1])
+        self.state = (ht, ct)
+        # x = x[:, -1, :]
+        logits = self.fc(x)  # logit from running x through linear layer
         return logits
 
     def configure_optimizers(self):
@@ -48,8 +50,9 @@ class LSTM1(pl.LightningModule):
 
     def _step(self, batch, batch_idx, logstring):
         data, label = batch
+        label = torch.cat([data[:,1:], label.unsqueeze(dim=1)], dim=1)
         logits = self(data)
-        loss = self.loss(logits, label)
+        loss = self.loss(logits.flatten(start_dim=0, end_dim=1), label.flatten(start_dim=0, end_dim=1))
         viewloss = self.viewloss(logits, label)
         tensorboard_logs = {'loss': {logstring: viewloss.detach()}}
         self.log("{} loss".format(logstring), viewloss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
@@ -65,8 +68,7 @@ class LSTM1(pl.LightningModule):
         return self._step(batch, batch_idx, "test")
 
 
-
-def test_hparam(hparam, values = [], logpath="./LSTM_logs/", tpu_cores=None, gpus=1):
+def test_hparam(hparam, values=[], logpath="./LSTM_logs/", tpu_cores=None, gpus=1):
     '''
 
     pretty neat function to test a hparam (as titled in LSTM1.__init__()) between a set of values.
@@ -88,12 +90,11 @@ def test_hparam(hparam, values = [], logpath="./LSTM_logs/", tpu_cores=None, gpu
     dataloader = wiki_dataloader(datasets=datasets, batch_size=64, num_workers=2)
 
     # default LSTM params
-    params = {'n_vocab':len(train.unique_tokens),
-              'embedding_size':100,
-              'num_layers':2,
-              'dropout':0,
-              'lr':1e-3}
-
+    params = {'n_vocab': len(train.unique_tokens),
+              'embedding_size': 100,
+              'num_layers': 2,
+              'dropout': 0,
+              'lr': 1e-3}
 
     for hparam_val in values:
 
@@ -102,11 +103,12 @@ def test_hparam(hparam, values = [], logpath="./LSTM_logs/", tpu_cores=None, gpu
 
         # Make model and train
         model = LSTM1(**params,
-                        trainweights = torch.log(1. / train.token_count()))
+                      trainweights=torch.log(1. / train.token_count()))
 
         tb_logger = pl_loggers.TensorBoardLogger(logpath, name="{}_{}".format(hparam, hparam_val))
         if hparam == 'gradient_clip_val':
-            trainer = pl.Trainer(gradient_clip_val=hparam_val, logger=tb_logger, max_epochs=20, tpu_cores=tpu_cores, gpus=gpus)
+            trainer = pl.Trainer(gradient_clip_val=hparam_val, logger=tb_logger, max_epochs=20, tpu_cores=tpu_cores,
+                                 gpus=gpus)
         else:
             trainer = pl.Trainer(gradient_clip_val=0, logger=tb_logger, max_epochs=20, tpu_cores=tpu_cores, gpus=gpus)
 
@@ -125,6 +127,7 @@ def test_hparam(hparam, values = [], logpath="./LSTM_logs/", tpu_cores=None, gpu
             print('{} ({}) [{}]'.format(sentence, nextword, nextpred))
     return
 
+
 if __name__ == '__main__':
     # Load datasets
     train = wiki_dataset('./wiki.train.txt', training=True, token_map='create', window=30)
@@ -134,3 +137,17 @@ if __name__ == '__main__':
 
     # Load dataloader
     dataloader = wiki_dataloader(datasets=datasets, batch_size=64, num_workers=2)
+
+    params = {'n_vocab': len(train.unique_tokens),
+              'embedding_size': 100,
+              'num_layers': 2,
+              'dropout': 0,
+              'lr': 1e-3}
+
+    model = LSTM1(**params,
+                  trainweights=torch.log(1. / train.token_count()))
+
+    tb_logger = pl_loggers.TensorBoardLogger('./Logs/', name="logname")
+    trainer = pl.Trainer(gradient_clip_val=0, logger=tb_logger, max_epochs=20, tpu_cores=None, gpus=None)
+
+    trainer.fit(model, dataloader)
