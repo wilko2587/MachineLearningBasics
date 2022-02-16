b9aabb5dc24744935b9106a6c66a5f894f84aec9
baljash-cheema <baljash-cheema@northwestern.edu>
2022-02-16 10:53:59 -0600

    Update rnn.py

--- NLP/HW2/rnn.py
+++ NLP/HW2/rnn.py
@@ -13,17 +13,18 @@ import time
 import matplotlib.pyplot as plt
 
 class rnn(pl.LightningModule):
-    def __init__(self, n_vocab, embedding_size, hidden_size, num_layers):
+    def __init__(self, n_vocab, embedding_size, hidden_size, num_layers, dropout, lr):
         super(rnn, self).__init__()
+        self.lr = lr
         self.embed = nn.Embedding(n_vocab, embedding_size)
         nn.init.uniform_(self.embed.weight, a=-0.1, b=0.1)
-        self.rnn = nn.RNN(input_size=embedding_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=False, dropout=0.2)
+        self.rnn = nn.RNN(input_size=embedding_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=False, dropout=dropout,lr=lr)
         nn.init.uniform_(self.rnn.weight_ih_l0, a=-0.1, b=0.1)
         nn.init.uniform_(self.rnn.weight_hh_l0, a=-0.1, b=0.1)
         nn.init.uniform_(self.rnn.weight_ih_l1, a=-0.1, b=0.1)
         nn.init.uniform_(self.rnn.weight_hh_l1, a=-0.1, b=0.1)
         self.fc = nn.Linear(hidden_size, n_vocab)
-        self.fc.weight = self.embed.weight
+        # self.fc.weight = self.embed.weight
         self.loss = nn.CrossEntropyLoss()
 
     def forward(self, x):
@@ -34,7 +35,7 @@ class rnn(pl.LightningModule):
         return logits
 
     def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=3e-3)
+        return optim.Adam(self.parameters(), lr=self.lr)
 
     def training_step(self, batch, batch_idx):
         data, label = batch
@@ -60,20 +61,61 @@ class rnn(pl.LightningModule):
         self.log("test loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         return {"loss": loss, "log": tensorboard_logs}
 
-if __name__ == '__main__':
+def test_hparam(hparam, values = [], logpath="./RNN_logs/", tpu_cores=None, gpus=1):
+    '''
+
+    pretty neat function to test a hparam (as titled in RNN.__init__()) between a set of values.
+    Will automatically log for you
+
+    hparam = string of hyperparam to vary
+    values = values of hparam to try
+    tpu_cores: either None, or 1 or 8
+    gpus = None, or integer
+    '''
+
     # Load datasets
-    train = wiki_dataset('wiki.train.txt', training=True, token_map='create', window=30)
-    valid = wiki_dataset('wiki.valid.txt', training=False, token_map=train.token_map, window=30)
-    test = wiki_dataset('wiki.test.txt', training=False, token_map=train.token_map, window=30)
+    train = wiki_dataset('./wiki.train.txt', training=True, token_map='create', window=30)
+    valid = wiki_dataset('./wiki.valid.txt', training=False, token_map=train.token_map, window=30)
+    test = wiki_dataset('./wiki.test.txt', training=False, token_map=train.token_map, window=30)
     datasets = [train, valid, test]
 
-    # Load dataloader
-    dataloader = wiki_dataloader(datasets=datasets, batch_size=20)
+    # default RNN params
+    params = {'n_vocab':len(train.unique_tokens),
+              'embedding_size':100,
+              'hidden_size':100,
+              'num_layers':2,
+              'dropout':0,
+              'lr':1e-3}
 
-    # Make model and train
-    model = rnn(n_vocab=len(train.unique_tokens), embedding_size=100, hidden_size=100, num_layers=2)
-    tb_logger = pl_loggers.TensorBoardLogger("./lightning_logs/", name="ff")
-    trainer = pl.Trainer(logger=tb_logger, max_epochs=20, gpus=1, callbacks=[EarlyStopping(monitor='val loss')])
-    trainer.fit(model, dataloader)
-    result = trainer.test(model, dataloader)
-    print(result)
+    # Load dataloader
+    dataloader = wiki_dataloader(datasets=datasets, batch_size=64, num_workers=2)
+
+    for hparam_val in values:
+
+        if hparam != 'gradient_clip_val':
+            params[hparam] = hparam_val
+
+        # Make model and train
+        model = RNN(**params,
+                        trainweights = torch.log(1. / train.token_count()))
+
+        tb_logger = pl_loggers.TensorBoardLogger(logpath, name="{}_{}".format(hparam, hparam_val))
+        if hparam == 'gradient_clip_val':
+            trainer = pl.Trainer(gradient_clip_val=hparam_val, logger=tb_logger, max_epochs=20, tpu_cores=tpu_cores, gpus=gpus)
+        else:
+            trainer = pl.Trainer(gradient_clip_val=0.5, logger=tb_logger, max_epochs=20, tpu_cores=tpu_cores, gpus=gpus)
+
+        trainer.fit(model, dataloader)
+        model.eval()
+        result = trainer.test(model, dataloader)
+        print('printing some example sentences from test set')
+        print('--> format: sentence (true) [predicted]')
+        for idx in np.random.randint(0, 1000, size=10):
+            features, groundTruth = test[idx]
+            fpass = model.forward(features.unsqueeze(dim=0))
+            pred = np.argmax(torch.softmax(fpass.detach().squeeze(dim=0), 0))
+            sentence = ''.join([test.decode_int(i) for i in features])
+            nextword = test.decode_int(groundTruth)
+            nextpred = test.decode_int(pred)
+            print('{} ({}) [{}]'.format(sentence, nextword, nextpred))
+    return
