17bef0c4af11320f351e2ecad68f2e3951f2aea1
baljash-cheema <baljash-cheema@northwestern.edu>
2022-02-16 20:57:42 -0600

    updates

--- NLP/HW2/rnn.py
+++ NLP/HW2/rnn.py
@@ -12,22 +12,28 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 import time
 import matplotlib.pyplot as plt
 
+
 class rnn(pl.LightningModule):
     def __init__(self, n_vocab, embedding_size, hidden_size, num_layers, dropout, lr, trainweights=None):
         super(rnn, self).__init__()
 
+        self.hidden_state = None
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+
         self.embed = nn.Embedding(n_vocab, embedding_size)
         nn.init.uniform_(self.embed.weight, a=-0.1, b=0.1)
 
-        self.rnn = nn.RNN(input_size=embedding_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=False, dropout=dropout)
+        self.rnn = nn.RNN(input_size=embedding_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=False,
+                          dropout=dropout)
+
         nn.init.uniform_(self.rnn.weight_ih_l0, a=-0.1, b=0.1)
         nn.init.uniform_(self.rnn.weight_hh_l0, a=-0.1, b=0.1)
         nn.init.uniform_(self.rnn.weight_ih_l1, a=-0.1, b=0.1)
         nn.init.uniform_(self.rnn.weight_hh_l1, a=-0.1, b=0.1)
 
         self.fc = nn.Linear(hidden_size, n_vocab)
-
-        # self.fc.weight = self.embed.weight
+        self.fc.weight = self.embed.weight  # tie embeddings
 
         self.lr = lr
         self.loss = nn.CrossEntropyLoss(weight=trainweights)
@@ -35,9 +41,15 @@ class rnn(pl.LightningModule):
 
     def forward(self, x):
         x = self.embed(x)
-        x, hidden = self.rnn(x)
-        x = x[-1, :, :]
+        if self.hidden_state == None:
+            x, hidden_state = self.rnn(x)
+        else:
+            x, hidden_state = self.rnn(x, self.hidden_state)
+
+        self.hidden_state = hidden_state.detach()
+
         logits = self.fc(x)
+
         return logits
 
     def configure_optimizers(self):
@@ -45,9 +57,10 @@ class rnn(pl.LightningModule):
 
     def _step(self, batch, batch_idx, logstring):
         data, label = batch
+        label = torch.cat([data[:, 1:], label.unsqueeze(dim=1)], dim=1)
         logits = self(data)
-        loss = self.loss(logits, label)
-        viewloss = self.viewloss(logits, label)
+        loss = self.loss(logits.flatten(start_dim=0, end_dim=1), label.flatten(start_dim=0, end_dim=1))
+        viewloss = self.viewloss(logits.flatten(start_dim=0, end_dim=1), label.flatten(start_dim=0, end_dim=1))
         tensorboard_logs = {'loss': {logstring: viewloss.detach()}}
         self.log("{} loss".format(logstring), viewloss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         return {"loss": loss, "log": tensorboard_logs}
