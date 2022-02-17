9fdd93a4659d6babb31e48b9da33950d04a76996
james-wilkinson-NW <91038008+james-wilkinson-NW@users.noreply.github.com>
2022-02-17 12:59:04 -0600

    Tiny thing -> added torch.seed(1) to LSTM so we can reproduce results on multiple runs

--- NLP/HW2/dataloader.py
+++ NLP/HW2/dataloader.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 import torch
 
+
 class wiki_dataloader(pl.LightningDataModule):
 
     def __init__(self, datasets, batch_size, num_workers=2, unk_threshold=1.):
@@ -22,18 +23,21 @@ class wiki_dataloader(pl.LightningDataModule):
         self.token_map = datasets[0].token_map
         self.batch_size = batch_size
 
-        self.train = [[each[0],each[1]] for each in self.train_dataset if torch.sum(each[0]==datasets[0].token_map['<unk>']) <= int(unk_threshold*datasets[0].window)]
-        self.val = [[each[0],each[1]] for each in self.valid_dataset if torch.sum(each[0]==datasets[0].token_map['<unk>']) <= int(unk_threshold*datasets[0].window)]
-        self.test = [[each[0],each[1]] for each in self.test_dataset if torch.sum(each[0]==datasets[0].token_map['<unk>']) <= int(unk_threshold*datasets[0].window)]
+        self.train = [[each[0], each[1]] for each in self.train_dataset if
+                      torch.sum(each[0] == datasets[0].token_map['<unk>']) <= int(unk_threshold * datasets[0].window)]
+        self.val = [[each[0], each[1]] for each in self.valid_dataset if
+                    torch.sum(each[0] == datasets[0].token_map['<unk>']) <= int(unk_threshold * datasets[0].window)]
+        self.test = [[each[0], each[1]] for each in self.test_dataset if
+                     torch.sum(each[0] == datasets[0].token_map['<unk>']) <= int(unk_threshold * datasets[0].window)]
 
     def train_dataloader(self):
         return DataLoader(self.train, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 
     def val_dataloader(self):
-        return DataLoader(self.val, batch_size=self.batch_size, shuffle=False, num_workers = self.num_workers)
+        return DataLoader(self.val, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
 
     def test_dataloader(self):
-        return DataLoader(self.test, batch_size=self.batch_size, shuffle=False, num_workers= self.num_workers)
+        return DataLoader(self.test, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
 
 
 if __name__ == "__main__":
@@ -41,8 +45,7 @@ if __name__ == "__main__":
     valid = wiki_dataset('./wiki.valid.txt', training=False, token_map=train.token_map, window=5)
     test = wiki_dataset('./wiki.test.txt', training=False, token_map=train.token_map, window=5)
 
-    datasets = [train,valid,test]
+    datasets = [train, valid, test]
 
     dataloader = wiki_dataloader(datasets=datasets, batch_size=64, unk_threshold=1)
-    #dataloader2 = wiki_dataloader(datasets=datasets, batch_size=64, unk_threshold=0.1)
-
+    # dataloader2 = wiki_dataloader(datasets=datasets, batch_size=64, unk_threshold=0.1)
